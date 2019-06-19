068cf58ddeb403569b28e96efe9795cbf5da6145
Ilya_Bat9 <nasirov@olympus.ru>
2019-06-19 21:09:34 +0500

    refactor

--- random_image_from_folder/templatetags/random_image.py
+++ random_image_from_folder/templatetags/random_image.py
@@ -6,18 +6,15 @@ from django.conf import settings
 register = template.Library()
 
 @register.simple_tag
-def random_image(image_dir):
+def random_image():
     try:
         valid_extensions = settings.RANDOM_IMAGE_EXTENSIONS
     except AttributeError:
         valid_extensions = ['.jpg','.jpeg','.png','.gif',]
 
-    if image_dir:
-        rel_dir = image_dir
-    else:
-        rel_dir = settings.RANDOM_IMAGE_DIR
-    rand_dir = os.path.join(settings.MEDIA_ROOT, rel_dir)
+    rel_dir = settings.RANDOM_IMAGE_DIR
+    rand_dir = os.path.join(settings.STATICFILES_DIRS[0], rel_dir)
 
     files = [f for f in os.listdir(rand_dir) if os.path.splitext(f)[1] in valid_extensions]
 
-    return os.path.join(rel_dir, random.choice(files))
+    return "/static" + "/" + settings.RANDOM_IMAGE_DIR + "/" + random.choice(files)
